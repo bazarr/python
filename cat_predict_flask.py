8ef4a74328efceb3cca3c3b7dd4798d204d6c7ae
Manuel Sabu <manuelntu@yahoo.com.sg>
2017-06-15 18:14:40 -0400

    normalized data and removed stopwords from input

--- cat_predict_flask.py
+++ cat_predict_flask.py
@@ -13,18 +13,19 @@ app = Flask(__name__)
 @app.route('/predict', methods =['GET'])
 
 def predict():
-    #print(stopwords.words("english"))
-    # Remove stopwords from text in the future...
     
     # Prediction text
     text = request.args.get('text') if request.args.get('text') else ""
+    # Split text using non alpha numeric, then remove stopwords
+    text = ' '.join([i for i in re.split('[^a-z0-9]', text.lower()) if i not in stop])
+    
     # Number of predictions
     predictions = int(request.args.get('num')) if request.args.get('num') else 3
 
     retval = dict()
     slicing_value = -1 * (predictions + 2)
     cat_train.insert(0, text)
-    tfidf = TfidfVectorizer(max_df=0.5, min_df=2, stop_words='english',
+    tfidf = TfidfVectorizer(max_df=0.4, min_df=2, stop_words='english', norm='l1',
                             use_idf=True).fit_transform(cat_train)
     cosine_similarities = linear_kernel(tfidf[0:1], tfidf).flatten()
     related_docs_indices = cosine_similarities.argsort()[-2:slicing_value:-1]
@@ -43,15 +44,19 @@ def predict():
 # Reading training data and doing cleanups
 cat_train = list()
 category = list()
+stop = set(stopwords.words('english'))
 
 def trainer(file):
     temp = ""
+    word_count = 0
     with open(file, 'rt') as csvfile:
         reader = csv.reader(csvfile)
         for index, row in enumerate(reader):
             if index is not 1:
                 row_words = re.sub("[^a-zA-Z -]", ' ', row[0].lower())
-                temp += ' ' + str(row_words)
+                if row_words not in stop:
+                    temp += ' ' + str(row_words)
+
     temp = re.sub('\s+', ' ', temp)
     cat_train.append(temp)
 
